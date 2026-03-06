6e4e4d42c9cda32d17343722d1dee389037f6962
d0uble0cash <cash.webb1@gmail.com>
2026-03-06 01:57:24 -0800

    Built item class added items moved architecture folders

---
+++ Assets/Scripts/Core/Items/ItemDefinition.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Items/Item Definition")]
+public class ItemDefinition : ScriptableObject
+{
+    [SerializeField] private string displayName;
+    public string DisplayName => displayName;
+    [SerializeField] private string id;
+    public string Id => id;
+    [SerializeField] private string description;
+    public string Description => description;
+    [SerializeField] private Sprite icon;
+    public Sprite Icon => icon;
+    [SerializeField] private int maxStackSize  = 99;   
+    public int MaxStackSize => maxStackSize;
+    [SerializeField] private int itemValue = 0;
+    public int ItemValue => itemValue;
+
+    private void OnValidate()
+    {
+        if (string.IsNullOrWhiteSpace(id))
+        {
+            Debug.LogWarning("ItemDefinition ID should not be empty!", this);
+        }        else if (id.Contains(" "))
+        {
+            Debug.LogWarning("ItemDefinition ID should not contain spaces!", this);
+        }
+        id = id.ToLowerInvariant();
+    }
+
+}
