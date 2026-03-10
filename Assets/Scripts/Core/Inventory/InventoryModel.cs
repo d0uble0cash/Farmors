625107240ba13cf49afeb16bc79141a4b7477163
d0uble0cash <cash.webb1@gmail.com>
2026-03-09 22:21:20 -0700

    Added inventory folder in core and boilerplate for inventory model

---
+++ Assets/Scripts/Core/Inventory/InventoryModel.cs
@@ -0,0 +1,12 @@
+public class InventoryModel
+{
+    // Dictionary<string, int>
+
+    // Add(string id, int amount)
+
+    // bool TryRemove(string id, int amount)
+
+    // int GetCount(string id)
+
+    // bool Has(string id, int amount)
+}
