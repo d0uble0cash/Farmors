736d92fbaf3f398ebaed8bc4c49ebc3fc1865ab6
Reese <reese.combs.529@my.csun.edu>
2026-04-30 12:13:48 -0700

    Inventory Class Assets and slot

---
+++ Assets/Scripts/Farmhub/UI/InventorySlot.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class InventorySlot : MonoBehaviour, IPointerClickHandler{
+    
+    //ITEMDATA//
+    public string itemName;
+    public Sprite itemSprite;
+    public bool isFull;
+    public bool isSelected = false;
+    public string itemDescription;
+    public int itemCost;
+    public Sprite emptySprite;
+    //ITEMDESCRIPTIONslot
+    public TMP_Text itemDescriptionNameText;
+    public TMP_Text itemDescriptionText;
+    public TMP_Text itemDescriptionCost;
+
+    //INVENTORYSLOT//
+    [SerializeField] private Image itemImage;
+    public void addItem(string itemName, Sprite itemSprite, string itemDescription, int itemCost){
+        this.itemName = itemName;
+        this.itemSprite = itemSprite;
+        this.itemDescription = itemDescription;
+        this.itemCost = itemCost;
+        isFull= true;
+
+        itemImage.sprite = itemSprite;
+    }
+
+    public void OnPointerClick(PointerEventData eventData){
+        if(eventData.button == PointerEventData.InputButton.Left){
+            //inventoryManager.DeselectAllSlots();
+            this.isSelected = true;
+            itemDescriptionNameText.text = itemName;
+            itemDescriptionText.text = itemDescription;
+            itemDescriptionCost.text = itemCost.ToString() + "ϵ";
+            if(itemDescriptionCost.text == null){
+                itemDescriptionCost.text = "";
+            }
+        }
+    }
+}
