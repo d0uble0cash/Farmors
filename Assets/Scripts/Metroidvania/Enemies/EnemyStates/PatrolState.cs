3059f15c66c521d00a271dbb361dbb86d6ff3ed7
bulgogipoop <albertchang0623@gmail.com>
2026-05-07 15:31:08 -0700

    Created enemy movement anim and scripts

--- Assets/Scripts/Metroidvania/Enemies/EnemyStates/PatrolState.cs
+++ Assets/Scripts/Metroidvania/Enemies/EnemyStates/PatrolState.cs
@@ -3,9 +3,14 @@ using UnityEngine;
 public class PatrolState : State
 {
     public PatrolState(Enemy enemy) : base(enemy) {}
+    protected override string AnimBoolName => "isWalking";
 
     public override void FixedUpdate()
     {
+        if(senses.GetChaseTarget()) {
+            stateMachine.ChangeState(new ChaseState(enemy));
+            return;
+        }
         if(senses.IsHittingWall() || senses.IsAtCliff())
         {
             enemy.Flip();
--- Assets/Scripts/Metroidvania/Enemies/Enemy_Senses.cs
+++ Assets/Scripts/Metroidvania/Enemies/Enemy_Senses.cs
@@ -7,10 +7,20 @@ public class Enemy_Senses : MonoBehaviour
 
     [SerializeField] private Transform groundCheck;
     [SerializeField] private Transform wallCheck;
+    [SerializeField] private Transform attackPoint;
 
     public bool IsAtCliff() => !Physics2D.Raycast(groundCheck.position, Vector2.down, config.groundCheckDistance, config.groundLayer);
-    public bool IsHittingWall() => !Physics2D.Raycast(wallCheck.position, Vector2.down, config.wallCheckDistance, config.wallLayer);
+    public bool IsHittingWall() => Physics2D.Raycast(wallCheck.position, Vector2.right, config.wallCheckDistance, config.wallLayer);
 
+    public Transform GetChaseTarget()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(attackPoint.position, config.chaseRange, config.targetLayer);
+        if(!hit)
+        {
+            return null;
+        }
+        return hit.transform;
+    }
     private void ODrawGizmosSelected()
     {
         Gizmos.color = Color.yellow;
@@ -18,5 +28,9 @@ public class Enemy_Senses : MonoBehaviour
 
         Gizmos.color = Color.blue;
         Gizmos.DrawLine(wallCheck.position, wallCheck.position + Vector3.right * enemy.FacingDirection * config.wallCheckDistance);
+    
+        //Chase Check
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPoint.position, config.chaseRange);
     }
 }
